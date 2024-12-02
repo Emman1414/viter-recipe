c3b47ed9d8ad7be669777f8e09f87d02566da0c6
liempot <leemagno9@gmail.com>
2024-12-02 15:47:36 +0800

    monday: not done

---
+++ src/components/pages/backend/recipe/ModalAddRecipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModalAddRecipe = () => {
+  return (
+    <div>ModalAddRecipe</div>
+  )
+}
+
+export default ModalAddRecipe
---
+++ src/components/pages/backend/recipe/Recipe.jsx
@@ -0,0 +1,52 @@
+import { setIsAdd } from "@/components/store/storeAction";
+import { StoreContext } from "@/components/store/storeContext";
+import { Plus } from "lucide-react";
+import React from "react";
+import Footer from "../partials/Footer";
+import Header from "../partials/Header";
+import SearchBar from "../partials/SearchBar";
+import SideNavigation from "../partials/SideNavigation";
+import ModalAddRecipe from "./ModalAddRecipe";
+import RecipeTable from "./RecipeTable";
+import ModalError from "../partials/modals/ModalError";
+
+const Recipe = () => {
+  const { dispatch, store } = React.useContext(StoreContext);
+  const [itemEdit, setItemEdit] = React.useState(null);
+
+  const handleAdd = () => {
+    dispatch(setIsAdd(true));
+    setItemEdit(null);
+  };
+  return (
+    <>
+      <section className="layout-main">
+        <div className="layout-division">
+          <SideNavigation menu="recipe" />
+          <main>
+            <Header title="Recipe" subtitle="Manage Recipe" />
+            <div className="p-8">
+              <div className="flex justify-between items-center ">
+                <SearchBar />
+
+                <button className="btn btn-add" onClick={handleAdd}>
+                  <Plus size={16} />
+                  Add New
+                </button>
+              </div>
+              <RecipeTable setItemEdit={setItemEdit} />
+            </div>
+            <Footer />
+          </main>
+        </div>
+      </section>
+
+      {store.validate && <ModalValidation />}
+      {store.error && <ModalError />}
+      {store.success && <ToastSuccess />}
+      {store.isAdd && <ModalAddRecipe itemEdit={itemEdit} />}
+    </>
+  );
+};
+
+export default Recipe;
